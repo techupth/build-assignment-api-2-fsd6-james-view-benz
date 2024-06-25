8a6d6547667f4c6f786efbf7373404b008ff7269
Wanchana <s.wanchana41@gmail.com>
2024-06-25 14:23:28 +0700

    feat add .env file to store database credential info

--- server/utils/db.mjs
+++ server/utils/db.mjs
@@ -1,10 +1,12 @@
 // Create PostgreSQL Connection Pool here !
 import * as pg from "pg";
+import dotenv from "dotenv";
+
 const { Pool } = pg.default;
+dotenv.config();
 
 const connectionPool = new Pool({
-  connectionString:
-    "postgresql://postgres:lastt1me@localhost:5432/Build Creating Data API Assignment",
+  connectionString: `postgresql://${process.env.DATABASE_USERNAME_KEY}:${process.env.DATABASE_PASSWORD_KEY}@${process.env.DATABASE_HOST_KEY}:${process.env.DATABASE_PORT_KEY}/${process.env.DATABASE_NAME_KEY}`,
 });
 
 export default connectionPool;
