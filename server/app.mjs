d177f9ad6a9613c7a01f83ce812faf8485497000
Wanchana <s.wanchana41@gmail.com>
2024-06-25 13:55:19 +0700

    fix: add app.use(express.json());

--- server/app.mjs
+++ server/app.mjs
@@ -3,9 +3,63 @@ import connectionPool from "./utils/db.mjs";
 
 const app = express();
 const port = 4001;
+app.use(express.json());
 
 app.get("/test", (req, res) => {
   return res.json("Server API is working 🚀");
+});
+
+app.post("/assignments", async (req, res) => {
+  const newAssignment = {
+    ...req.body,
+    create_at: new Date(),
+    updated_at: new Date(),
+    published_at: new Date(),
+    status: "publish",
+    length: "short",
+    title: "test",
+  };
+
+  const requiredFields = ["content", "category"];
+  for (const field of requiredFields) {
+    if (!newAssignment[field]) {
+      return res.status(400).json({
+        message: `Server could not create assignment because there are missing data from client: ${field} is required`,
+      });
+    }
+  }
+
+  console.log(newAssignment);
+
+  try {
+    const queryText = `
+      INSERT INTO assignments (user_id, title, content, category, length, status, created_at, updated_at, published_at)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+    `;
+
+    const values = [
+      1,
+      newAssignment.title,
+      newAssignment.content,
+      newAssignment.category,
+      newAssignment.length,
+      newAssignment.status,
+      newAssignment.create_at,
+      newAssignment.updated_at,
+      newAssignment.published_at,
+    ];
+
+    await connectionPool.query(queryText, values);
+
+    return res.status(201).json({
+      message: "Created assignment successfully",
+    });
+  } catch (error) {
+    console.error("Error inserting assignment:", error);
+    return res.status(500).json({
+      message: "Server could not create assignment because of a database error",
+    });
+  }
 });
 
 app.get("/assignments", async (req, res) => {
